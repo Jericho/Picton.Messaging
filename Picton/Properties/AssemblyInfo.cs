9dca576af7d72a50ad093339578e36510b07ab9b
Jeremie Desautels <desautelsj@hotmail.com>
2016-04-04 09:47:12 -0400

    Add description in AssemblyInfo.cs

--- Picton/Properties/AssemblyInfo.cs
+++ Picton/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Picton")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("High performance Azure worker role")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeremie Desautels")]
 [assembly: AssemblyProduct("Picton")]
