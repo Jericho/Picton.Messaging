2971e13f85b6f7c3a20773b5f3e0e7db0ae6d44f
Jericho <desautelsj@hotmail.com>
2024-12-30 11:33:39 -0500

    Guard against null

--- Source/Picton.Messaging/Metrics.cs
+++ Source/Picton.Messaging/Metrics.cs
@@ -13,16 +13,19 @@ namespace Picton.Messaging
 				typeof(AsyncMessagePump).GetTypeInfo().Assembly.GetName().Version.ToString(3);
 #endif
 
-			var meter = meterFactory.Create("Picton.Messaging", pictonMessagingVersion);
+			if (meterFactory != null)
+			{
+				var meter = meterFactory.Create("Picton.Messaging", pictonMessagingVersion);
 
-			MessagesProcessed = meter.CreateCounter<int>("picton.messaging.messages_processed", null, "The number of messages processed by the message pump.");
-			MessageWaitBeforeProcess = meter.CreateHistogram<long>("picton.messaging.message_wait_before_process", null, "How long a message was in queue, waiting to be processed.");
-			MessageProcessing = meter.CreateHistogram<long>("picton.messaging.message_processing", null, "The time it takes to process a message.");
-			MessagesFetching = meter.CreateHistogram<long>("picton.messaging.messages_fetching", null, "The time it takes to fetch a batch of messages from the Azure queue.");
-			QueueEmpty = meter.CreateCounter<int>("picton.messaging.queue_empty", null, "The number of times we attempted to fetch messages from an Azure queue but it was empty.");
-			AllQueuesEmpty = meter.CreateCounter<int>("picton.messaging.all_queues_empty", null, "The number of times we attempted to fetch messages from Azure but all the queues are empty.");
-			QueuedCloudMessages = meter.CreateHistogram<int>("picton.messaging.queued_cloud_messages", null, "The number of messages waiting in the Azure queue over time.");
-			QueuedMemoryMessages = meter.CreateHistogram<int>("picton.messaging.queued_memory_messages", null, "The number of messages waiting in the memory queue over time.");
+				MessagesProcessed = meter.CreateCounter<int>("picton.messaging.messages_processed", null, "The number of messages processed by the message pump.");
+				MessageWaitBeforeProcess = meter.CreateHistogram<long>("picton.messaging.message_wait_before_process", null, "How long a message was in queue, waiting to be processed.");
+				MessageProcessing = meter.CreateHistogram<long>("picton.messaging.message_processing", null, "The time it takes to process a message.");
+				MessagesFetching = meter.CreateHistogram<long>("picton.messaging.messages_fetching", null, "The time it takes to fetch a batch of messages from the Azure queue.");
+				QueueEmpty = meter.CreateCounter<int>("picton.messaging.queue_empty", null, "The number of times we attempted to fetch messages from an Azure queue but it was empty.");
+				AllQueuesEmpty = meter.CreateCounter<int>("picton.messaging.all_queues_empty", null, "The number of times we attempted to fetch messages from Azure but all the queues are empty.");
+				QueuedCloudMessages = meter.CreateHistogram<int>("picton.messaging.queued_cloud_messages", null, "The number of messages waiting in the Azure queue over time.");
+				QueuedMemoryMessages = meter.CreateHistogram<int>("picton.messaging.queued_memory_messages", null, "The number of messages waiting in the memory queue over time.");
+			}
 		}
 
 		/// <summary>
