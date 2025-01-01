ddcac15a63933b91f6cb189d42eb327385400200
Jericho <desautelsj@hotmail.com>
2025-01-01 10:05:02 -0500

    Remove unused "using" statement

--- Source/Picton.Messaging.IntegrationTests/Program.cs
+++ Source/Picton.Messaging.IntegrationTests/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.Metrics;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Prometheus;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
